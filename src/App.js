3c7def170127e8fcc1c716d0c92989e50516fd6d
jnana sai ande <127129986+jnanasai420@users.noreply.github.com>
2025-03-06 19:40:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Intro from "./Intro";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Intro />
+    </div>
+  );
+}
---
+++ src/Intro.js
@@ -0,0 +1,36 @@
+const Intro = () => {
+  return (
+    <>
+      <div>
+        <table border={1}>
+          <tr>
+            <th>Name</th>
+            <th>Relation</th>
+            <th>Phno</th>
+          </tr>
+          <tr>
+            <td>Vighneswararao</td>
+            <td>father</td>
+            <td>900000000</td>
+          </tr>
+          <tr>
+            <td>Subba Lakshmi</td>
+            <td>Mother</td>
+            <td>8000000000</td>
+          </tr>
+          <tr>
+            <td>Jnana sai</td>
+            <td>Son</td>
+            <td>7000000000</td>
+          </tr>
+          <tr>
+            <td>Bhavya</td>
+            <td>Daughter</td>
+            <td>9000000000</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+};
+export default Intro;
